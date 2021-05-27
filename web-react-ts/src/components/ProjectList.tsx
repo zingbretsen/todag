90b9eea063a56cf1548a878b6243d90c664e4332
Zach Ingbretsen <zach@zingbretsen.com>
2021-05-27 09:25:31 -0400

    Removes sorting of projects

--- web-react-ts/src/components/ProjectList.tsx
+++ web-react-ts/src/components/ProjectList.tsx
@@ -2,108 +2,88 @@ import React from 'react'
 
 import { withStyles, createStyles, Theme } from '@material-ui/core/styles'
 import {
-  Table,
-  TableBody,
-  TableCell,
-  SortDirection,
-  TableHead,
-  TableRow,
-  Tooltip,
-  Paper,
-  TableSortLabel,
-  TextField,
+    Table,
+    TableBody,
+    TableCell,
+    TableRow,
+    Paper,
+    TextField,
 } from '@material-ui/core'
 import { useQuery, gql } from '@apollo/client'
 
 import Title from './Title'
 
 const styles = (theme: Theme) =>
-  createStyles({
-    root: {
-      maxWidth: 700,
-      marginTop: theme.spacing(3),
-      overflowX: 'auto',
-      margin: 'auto',
-    },
-    table: {
-      minWidth: 700,
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      minWidth: 300,
-    },
-  })
+    createStyles({
+        root: {
+            maxWidth: 700,
+            marginTop: theme.spacing(3),
+            overflowX: 'auto',
+            margin: 'auto',
+        },
+        table: {
+            minWidth: 700,
+        },
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            minWidth: 300,
+        },
+    })
 
 const GET_PROJECT = gql`
-  query projectsPaginateQuery(
-    $first: Int
-    $offset: Int
-    $orderBy: [ProjectSort]
-    $filter: ProjectWhere
-  ) {
-    projects(
-      options: { limit: $first, skip: $offset, sort: $orderBy }
-      where: $filter
+    query projectsPaginateQuery(
+        $first: Int
+        $offset: Int
+        $filter: ProjectWhere
     ) {
-      name
-      nextActions {
-        name
-      }
+        projects(
+            options: { limit: $first, skip: $offset }
+            where: $filter
+        ) {
+            name
+            nextActions {
+                name
+            }
+        }
     }
-  }
 `
 
 function ProjectList(props: any) {
-  const { classes } = props
-  const [order, setOrder] = React.useState<'asc' | 'desc'>('asc')
-  const [orderBy, setOrderBy] = React.useState('name')
-  const [page] = React.useState(0)
-  const [rowsPerPage] = React.useState(10)
-  const [filterState, setFilterState] = React.useState({ projectnameFilter: '' })
+    const { classes } = props
+    const [page] = React.useState(0)
+    const [rowsPerPage] = React.useState(10)
+    const [filterState, setFilterState] = React.useState({ projectnameFilter: '' })
 
-  const getFilter = () => {
-    return filterState.projectnameFilter.length > 0
-      ? { name_CONTAINS: filterState.projectnameFilter }
-      : {}
-  }
-
-  const { loading, data, error } = useQuery(GET_PROJECT, {
-    variables: {
-      first: rowsPerPage,
-      offset: rowsPerPage * page,
-      orderBy: { [orderBy]: order.toUpperCase() },
-      filter: getFilter(),
-    },
-  })
-
-  const handleSortRequest = (property: any) => {
-    const newOrderBy = property
-    let newOrder: SortDirection = 'desc'
-
-    if (orderBy === property && order === 'desc') {
-      newOrder = 'asc'
+    const getFilter = () => {
+        return filterState.projectnameFilter.length > 0
+                                                    ? { name_CONTAINS: filterState.projectnameFilter }
+                                                    : {}
     }
 
-    setOrder(newOrder)
-    setOrderBy(newOrderBy)
-  }
+    const { loading, data, error } = useQuery(GET_PROJECT, {
+        variables: {
+            first: rowsPerPage,
+            offset: rowsPerPage * page,
+            filter: getFilter(),
+        },
+    })
 
-  const handleFilterChange = (filterName: any) => (event: any) => {
-    const val = event.target.value
+    const handleFilterChange = (filterName: any) => (event: any) => {
+        const val = event.target.value
 
-    setFilterState((oldFilterState) => ({
-      ...oldFilterState,
-      [filterName]: val,
-    }))
-  }
+        setFilterState((oldFilterState) => ({
+            ...oldFilterState,
+            [filterName]: val,
+        }))
+    }
 
 
-  return (
-    <>
+    return (
+        <>
         <Paper className={classes.root}>
-      <Title>Project List</Title>
-      <TextField
+        <Title>Project List</Title>
+        <TextField
         id="search"
         label="Project Name Contains"
         className={classes.textField}
@@ -113,57 +93,39 @@ function ProjectList(props: any) {
         variant="outlined"
         type="text"
         InputProps={{
-          className: classes.input,
+            className: classes.input,
         }}
-      />
-      {loading && !error && <p>Loading...</p>}
-      {error && !loading && <p>Error</p>}
-      {data && !loading && !error && <> {
-              data.projects.map((n: any) => {
-                  return (
-                      <div key={n.id}>
-                          <h2>{n.name}</h2>
-                      <Table className={classes.table}>
-                          <TableHead>
-                              <TableRow>
-                                  <TableCell
-                                      key="name"
-                                      sortDirection={orderBy === 'name' ? order : false}
-                                  >
-                                      <Tooltip title="Sort" placement="bottom-start" enterDelay={300}>
-                                          <TableSortLabel
-                                              active={orderBy === 'name'}
-                                              direction={order}
-                                              onClick={() => handleSortRequest('name')}
-                                          >
-                                              Name
-                                          </TableSortLabel>
-                                      </Tooltip>
-                                  </TableCell>
-                              </TableRow>
-                          </TableHead>
-                          <TableBody>
-                              {n.nextActions.map((m: any) => {
-                                      return (
-                                          <TableRow key={n.id + m.name}>
-                                              <TableCell component="th" scope="row">
-                                                  {m.name}
-                                              </TableCell>
-                                          </TableRow>
-                                      )
-                                  })
-                              }
-                          </TableBody>
-                      </Table>
-                      </div>
-                  )
-              }
-              )
-      } </>
-      }
+        />
+        {loading && !error && <p>Loading...</p>}
+        {error && !loading && <p>Error</p>}
+        {data && !loading && !error && <> {
+            data.projects.map((n: any) => {
+                return (
+                    <div key={n.id}>
+                        <h2>{n.name}</h2>
+                        <Table className={classes.table}>
+                            <TableBody>
+                                {n.nextActions.map((m: any) => {
+                                    return (
+                                        <TableRow key={n.id + m.name}>
+                                            <TableCell component="td" scope="row">
+                                                {m.name}
+                                            </TableCell>
+                                        </TableRow>
+                                    )
+                                })
+                                }
+                            </TableBody>
+                        </Table>
+                    </div>
+                )
+            }
+            )
+        } </>
+        }
       </Paper>
-      </>
-  )
+        </>
+    )
 }
 
 export default withStyles(styles)(ProjectList)
